394f9c69467ced493e0ff30f9aadf2456bc3e6ed
Krishna Singh <singhkrishna5099213@gmail.com>
2025-04-02 10:09:29 +0530

    5th commit

---
+++ StringPW/StringCompression.java
@@ -0,0 +1,33 @@
+package StringPW;
+
+public class StringCompression {
+    public static void main(String[] args) {
+        String s = "aaaaaaabbbbvvvvsssswwwwqww";
+        char [] arr = s.toCharArray();
+        String ans = "";
+        int i = 0 , j=0;
+        while(j<arr.length){
+            if(arr[j]==arr[i]){
+                j++;
+            }
+            else{
+                ans+=arr[i];
+                int len = j-i;
+                if(len>1 ){
+                    ans+=len;
+                
+                }
+                i=j;
+            }
+        }
+        ans+=arr[i];
+        int len = j-i;
+        if(len>1 ){
+            ans+=len;
+        }
+        i=j;
+        System.out.println(ans);
+
+
+    }
+}
