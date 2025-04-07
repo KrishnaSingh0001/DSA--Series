ee7eafbca252c1d9e7d1cca56856cbb224077713
Krishna Singh <singhkrishna5099213@gmail.com>
2025-04-07 14:07:56 +0530

    Stocks Buy Sell Commit

---
+++ Array/StocksBuySell.java
@@ -0,0 +1,20 @@
+public class StocksBuySell {
+    public static int maxpro(int [] arr) {
+        int maxpro =0 ;
+        for(int i = 0 ; i < arr.length; i++){
+            for(int j=i ; j< arr.length; j++){
+                if(arr[j] > arr[i]){
+                    // maxpro = Math.max(maxpro, arr[i] , arr[i-1]);
+                    int profit = arr[j] - arr[i];
+                    maxpro = Math.max(maxpro, profit);
+                }
+            }
+        }
+        return maxpro;
+    }
+    public static void main(String[] args) {
+        int arr [] = {7,1,5,3,6,4};
+        
+        System.out.println("Max Profit is : " + maxpro(arr));
+    }
+}
