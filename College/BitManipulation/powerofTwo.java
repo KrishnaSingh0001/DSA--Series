710cfac7214929f600bec128d95f88d619f96d9e
Krishna Singh <singhkrishna5099213@gmail.com>
2025-04-07 15:45:30 +0530

    Bit manipulation

---
+++ College/BitManipulation/powerofTwo.java
@@ -0,0 +1,26 @@
+package College.BitManipulation;
+import java.util.*;
+public class powerofTwo {
+    public static boolean ispower(int a ) {
+      
+        if (a <= 0){  // base case 
+            return false;
+        }  
+
+        // Step 2: Use bit manipulation
+        return (a & (a - 1)) == 0;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number : ");
+        int a = sc.nextInt();
+        // int b = sc.nextInt();
+        if(ispower(a)){
+            System.out.println("yes");
+        }
+        else{
+            System.out.println("No");
+        }
+    }
+    
+}
