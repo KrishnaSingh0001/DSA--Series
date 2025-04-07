ba8c027ddfbc8a2c1e45a86f2d5fa052d222dc4e
Krishna Singh <singhkrishna5099213@gmail.com>
2025-04-07 09:41:13 +0530

    6th Commit

---
+++ PatternPrinting/alphabeticPattern.java
@@ -0,0 +1,18 @@
+package PatternPrinting;
+
+import java.util.Scanner;
+
+public class alphabeticPattern {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+        System.out.println("Enter N: ");
+        int n = sc.nextInt();
+        for(int i = 1 ; i <=n ; i++){
+            for(int j = 1 ; j<=n ; j++){
+                // System.out.print((char)(i+64)+" "); // for AAAA, BBBB , CCCC , DDDD
+                System.out.print((char)(j+64)+" ");
+            }
+            System.out.println();
+        }
+    }
+}
