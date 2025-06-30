3f0267723b219ed3041cae0bfb001a0a22aa24de
Krishna Singh <singhkrishna5099213@gmail.com>
2025-06-30 15:23:41 +0530

    Day2 Linked List

---
+++ Linked_List/implementationLL.java
@@ -0,0 +1,9 @@
+package Linked_List;
+import java.util.*;
+public class implementationLL {
+    public static void main(String[] args) {
+        ArrayList<Integer> arr = new ArrayList<>();
+
+        
+    }
+}
