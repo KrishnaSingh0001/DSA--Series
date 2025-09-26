b33b04811a472b7933ac392f1285a0140c5f9ea7
KrishnaSingh0001 <singhkrishna5099213@gmail.com>
2025-09-26 19:51:31 +0530

    changes

--- ArraysQuestion/RemoveDuplicateElement.java
+++ ArraysQuestion/RemoveDuplicateElement.java
@@ -3,19 +3,17 @@ package ArraysQuestion;
 public class RemoveDuplicateElement {
     public static void main(int arr[]) {
         int n = arr.length;
-        int index=0;
+        int idx=0;
         for (int i = 1; i < arr.length; i++) {
-            if (arr[i] != arr[index]) {
-                index++;
-                arr[index] = arr[i]; 
+            if (arr[i] != arr[idx]) {
+                idx++;
+                arr[idx] = arr[i]; 
             }
         }
-        return index + 1;
+        return idx + 1;
     }
     public static void main(String[] args) {
         int arr[] = {1, 1 , 2,2,3,3};
         System.out.println(arr);
-        
-
     }
 }
